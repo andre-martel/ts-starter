b7ff518379d2f51c71152974fe10fba5f61c82e4
KendoSensei <erwan.beuselinck@gmail.com>
2024-10-11 15:16:59 +0200

    feur

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,11 @@
-import express from "express";
+import express, { Request, Response } from "express";
 
 const application = express();
+
+application.get("/", (_request: Request, response: Response) => {
+  return response.json({
+    message: "OK",
+  });
+});
 
 application.listen(3000, () => console.log("c'est good !"));
